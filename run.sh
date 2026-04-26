749713e47844b947111110ad176fd613ded9f128
Joshua A Hull <joshhull911@ayhoo.com>
2026-04-26 12:02:07 +0000

    Rename script to Library_Reader.py; update README per author

--- run.sh
+++ run.sh
@@ -10,19 +10,19 @@ mkdir -p out
 python3 simulate_reads.py --out-dir data --left "$LEFT" --right "$RIGHT"
 
 echo "--- FASTA, no Q ---"
-python3 count_between_barcodes.py data/reads.fasta \
+python3 Library_Reader.py data/reads.fasta \
     --left "$LEFT" --right "$RIGHT" \
     --out      out/counts_fasta.tsv \
     --metadata out/counts_fasta.meta.txt
 
 echo "--- FASTQ, mean Q >= 20 ---"
-python3 count_between_barcodes.py data/reads.fastq \
+python3 Library_Reader.py data/reads.fastq \
     --left "$LEFT" --right "$RIGHT" --min-mean-q 20 \
     --out      out/counts_meanQ20.tsv \
     --metadata out/counts_meanQ20.meta.txt
 
 echo "--- FASTQ, per-position Q >= 15 ---"
-python3 count_between_barcodes.py data/reads.fastq \
+python3 Library_Reader.py data/reads.fastq \
     --left "$LEFT" --right "$RIGHT" --min-pos-q 15 \
     --out      out/counts_posQ15.tsv \
     --metadata out/counts_posQ15.meta.txt
